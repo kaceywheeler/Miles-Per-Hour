ec99742efe4288329f01d3fda430bb9e83d71993
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-06 12:12:38 -0700

    Update MilesPerHour.java

--- MilesPerHour.java
+++ MilesPerHour.java
@@ -3,9 +3,16 @@ import java.lang.Math;
 
 public class MilesPerHour {
 	
+	//Method to calculate speed in miles per hour
 	public int mph(int d, int hrs, int min) {
+		
+		//Find the total time in hours (convert minutes to hours and add it to hours)
 		double hrs2 = hrs + (min/60);
+		
+		//Calculate speed
 		int speed = (int)(d / hrs2);
+		
+		//Return speed
 		return speed;
 		
 		
