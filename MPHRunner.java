709b76b07d79d0139b16eb45f9b194cd6de892b8
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-06 12:10:22 -0700

    Update MPHRunner.java

--- MPHRunner.java
+++ MPHRunner.java
@@ -6,17 +6,22 @@ public class MPHRunner {
 
 	public static void main(String[] args) {
 		
+		//Create new Scanner
 		Scanner keyboard = new Scanner(System.in);
 		
+		//Get value for distance
 		System.out.println("Enter the distance: ");
 		int d = keyboard.nextInt();
 		
+		//Get value for hours
 		System.out.println("Enter the hours: ");
 		int hrs = keyboard.nextInt();
 		
+		//Get value for minutes
 		System.out.println("Enter the minutes: ");
 		int min = keyboard.nextInt();
 		
+		//Print out all variables and speed
 		System.out.println(d + " miles in " + hrs + " hours and " + min + " minutes = " + speed + " MPH");
 		
 
